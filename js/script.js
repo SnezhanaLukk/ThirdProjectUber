9cbfd9d0497e616f8a93490a4986039d024905de
SnezhanaLukk <157698876+SnezhanaLukk@users.noreply.github.com>
2024-11-25 14:28:12 +0300

    Add modal window

--- js/script.js
+++ js/script.js
@@ -15,3 +15,31 @@ window.addEventListener('DOMContentLoaded', ()=>{
         })
     })
 });
+// modal dialog
+ const modalDialog = document.querySelector('.modal'),
+    modalOpenBtn = document.querySelectorAll('[data-modal]');
+
+modalOpenBtn.forEach(btn => {
+    btn.addEventListener('click', openModal);
+});
+
+modalDialog.addEventListener('click', e => {
+    if (e.target === modalDialog || e.target.getAttribute('data-close') == '')
+        closeModal();
+});
+
+document.addEventListener('keydown', e =>{
+    if (e.target === "Escape" &&  modalDialog.classList.contains('show'))
+        closeModal();
+})
+
+function openModal(){
+    modalDialog.classList.remove('hide');
+    modalDialog.classList.add('show');
+}
+
+function closeModal(){
+    modalDialog.classList.remove('show');
+    modalDialog.classList.add('hide');
+}
+    
